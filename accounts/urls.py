64c599940328344f504974a0795c978e98b0b4e5
Sandhis <sandhismurugesan@gmail.com>
2020-07-27 00:27:32 +0530

    Add registration view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from .views import (
-    login_view,
+    login_view, register_view,
 )
 
 urlpatterns = [
     url(r"^login/$", login_view, name = "login"),
+    url(r"^register/$", register_view, name = "register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -6,18 +6,34 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.urls import reverse
 
-from .forms import UsersLoginForm
+from .forms import UsersLoginForm, UsersRegisterForm
+
 
 def login_view(request):
-	form = UsersLoginForm(request.POST or None)
-	if form.is_valid():
-		username = form.cleaned_data.get("username")
-		password = form.cleaned_data.get("password")
-		user = authenticate(username = username, password = password)
-		login(request, user)
-		return redirect(reverse("videos:list"))
-	return render(request, "accounts/form.html", {
-		"form" : form,
-		"title" : "Login",
-	})
+    form = UsersLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        return redirect(reverse("videos:list"))
+    return render(request, "accounts/form.html", {
+        "form": form,
+        "title": "Login",
+    })
 
+
+def register_view(request):
+    form = UsersRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save()
+        password = form.cleaned_data.get("password")
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        return redirect("/accounts/login")
+    return render(request, "accounts/form.html", {
+        "title": "Register",
+        "form": form,
+    })
