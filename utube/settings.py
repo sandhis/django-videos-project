d11b59f45b88da2932edb3f8a1feabc7fbeef19e
Sandhis <sandhismurugesan@gmail.com>
2020-07-26 16:26:02 +0530

    Change settings

--- utube/settings.py
+++ utube/settings.py
@@ -1,7 +1,7 @@
 import os
 import dj_database_url
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['django-utube.herokuapp.com']
 
@@ -116,14 +116,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
-MEDIA_URL= "/media/"
-LOGIN_REDIRECT_URL = '/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = "/media/"
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATIC_URL = '/static/'
-
 STATIC_FILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATICFILES_DIRS = [os.path.join(PROJECT_ROOT, "staticfiles")]
