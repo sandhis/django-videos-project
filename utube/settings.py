dbe9c17fea4432b76872f18843dfa0be232193cb
Sandhis <sandhismurugesan@gmail.com>
2020-07-26 14:13:23 +0530

    Fix whitenose settings

--- utube/settings.py
+++ utube/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
