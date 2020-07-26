a2d6b2a0c51ff2c0e0750f05782fa2d93d185c45
Sandhis <sandhismurugesan@gmail.com>
2020-07-26 14:49:25 +0530

    Add setting

--- utube/settings.py
+++ utube/settings.py
@@ -119,3 +119,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
 LOGIN_REDIRECT_URL = '/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
