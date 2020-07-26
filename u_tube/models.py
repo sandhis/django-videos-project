4c28dcacb84ce8cd8f28a0edbab372d641032905
Sandhis <sandhismurugesan@gmail.com>
2020-07-26 17:23:02 +0530

    Alignment

--- u_tube/models.py
+++ u_tube/models.py
@@ -6,9 +6,8 @@ class Video(models.Model):
     title = models.CharField(null=False, max_length=255)
     description = models.TextField(null=True, blank=True)
     video = models.FileField(upload_to='videos/')
-    thumbnail = models.FileField(upload_to='video_thumb/',default='')
+    thumbnail = models.FileField(upload_to='video_thumb/', default='')
     pub_date = models.DateTimeField(default=now())  
-
 
     def __str__(self):
         return self.title
